e7763ef0c4ab9c24682bee9b6ce80a303dbfe787
Alenriquez96 <alberto.enriquez96@gmail.com>
2022-04-19 19:57:59 +0200

    neas finished

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express =  require("express");
-const landingRouter = require("./routes/landingsRoutes")
+const landingRouter = require("./routes/landingsRoutes");
+const neasRouter = require("./routes/neasRoutes");
 const app = express();
 const port = 3000;
 
@@ -11,7 +12,8 @@ app.use(express.static("public"));
 app.use(express.urlencoded({extended:true}));//Estas dos son para los métodos put y post, para que el servidor pueda leer la información nueva que le mandamos
 app.use(express.json());
 
-app.use("/api",landingRouter)
+app.use("/api",landingRouter);
+app.use("/api/astronomy",neasRouter);
 
 
 app.listen(port, ()=>{
---
+++ models/neasModel.js
@@ -0,0 +1,93 @@
+const schema = require("./neasSchema");
+
+const getOrbitClass = async (orbit_class)=>{
+    try {
+        const getOC = await schema.find({orbit_class: orbit_class},"designation period_yr -_id")
+        return getOC;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const getAllNeas = async ()=>{
+    try {
+        const getAll = await schema.find({});
+        return getAll;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const getNeasYears = async(years)=>{
+    try {
+        console.log(years);
+        const neasYears = await schema.find({discovery_date:{$gte: years.from, $lt: years.to}}, "designation discovery_date period_yr -_id");
+        return neasYears; 
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const getNeasYearsFrom = async(years)=>{
+    try {
+        console.log(years.from);
+        const neasYears = await schema.find({discovery_date:{$gte: years.from}}, "designation discovery_date period_yr -_id");
+        return neasYears; 
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const getNeasYearsTo = async(years)=>{
+    try {
+        console.log(years.to);
+        const neasYears = await schema.find({discovery_date:{$lt: years.to}}, "designation discovery_date period_yr -_id");
+        return neasYears; 
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const createNea = async(nea)=>{
+    try {
+        const newNea = new schema(nea);
+        const create = await schema.create(newNea)
+        await create.save();
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const updateNea = async (designation)=>{
+    try {
+        const oId = new ObjectId(designation);
+        const neaToUpdate = await schema.find({designation: oId});
+        const updatedNea = new schema(req.body);
+        neaToUpdate.overwrite(updatedNea);
+        await neaToUpdate.save();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const deleteNea = async (designation)=>{
+    try {
+        console.log(designation);
+        await schema.deleteOne({designation: designation});
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const exp = {
+    getOrbitClass,
+    getAllNeas,
+    getNeasYears,
+    getNeasYearsFrom,
+    getNeasYearsTo,
+    createNea,
+    updateNea,
+    deleteNea
+}
+
+module.exports = exp;
