8bd40263be6ecbc4e6cd3d831bd4e319d7db8e7c
Alenriquez96 <alberto.enriquez96@gmail.com>
2022-04-21 16:21:11 +0200

    deploy

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express =  require("express");
 const landingRouter = require("./routes/landingsRoutes");
 const neasRouter = require("./routes/neasRoutes");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 5000;
 
 
 app.set("view engine", "pug");
