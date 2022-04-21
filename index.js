6b051762af5a664c9a527ad9218bce43c4eee01b
Alenriquez96 <alberto.enriquez96@gmail.com>
2022-04-21 15:57:30 +0200

    finished

--- index.js
+++ index.js
@@ -5,10 +5,6 @@ const app = express();
 const port = 3000;
 
 
-app.set("view engine", "pug");
-app.set("views", "./views");
-app.use(express.static("public"));
-
 app.use(express.urlencoded({extended:true}));//Estas dos son para los métodos put y post, para que el servidor pueda leer la información nueva que le mandamos
 app.use(express.json());
 
