6cd40731b0ae859e64307999ccabe2788cde4dbe
alenriquez96 <alberto.enriquez96@gmail.com>
2022-05-08 22:46:34 +0200

    add things

--- client/src/components/Main/Landings/Landings.jsx
+++ client/src/components/Main/Landings/Landings.jsx
@@ -1,11 +1,39 @@
 import React from "react";
+import { MapContainer, TileLayer, Marker, Popup} from 'react-leaflet';
+import axios from 'axios';
+import 'leaflet/dist/leaflet.css';  
+import "./Landings.css";
+
 
 const Landings = () => {
+
+  const fetchData = async () =>{
+    try {
+      const res = await axios.get("/api/astronomy/landings")
+      const data =  await res.data
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  } 
+  fetchData();
 
 
 
   return (
-    <div></div>
+    <div>
+      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
   )
 }
 
