b84f8e7bf1f746d6ac83709268d12510cf5bbe46
Alenriquez96 <alberto.enriquez96@gmail.com>
2022-05-17 16:33:12 +0200

    configure

--- index.js
+++ index.js
@@ -5,11 +5,17 @@ const usersRouter = require("./routes/usersRoutes");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
+const path = require("path")
 
 
 app.use(express.urlencoded({extended:true}));//Estas dos son para los métodos put y post, para que el servidor pueda leer la información nueva que le mandamos
 app.use(express.json());
 app.use(cors());
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('/', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 app.use("/api",landingRouter);
 app.use("/api/astronomy",neasRouter);
