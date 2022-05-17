1e68fb2ad2bcef09f43b92644db3e86ca1443bbe
Alenriquez96 <alberto.enriquez96@gmail.com>
2022-05-17 16:52:43 +0200

    added

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ app.use(express.json());
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-function(req, res) {
-  res.sendFile(path.join(__dirname, '/client/build', 'index.html'))
-}
+app.get('/', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  });
 
 app.use("/api",landingRouter);
 app.use("/api/astronomy",neasRouter);
